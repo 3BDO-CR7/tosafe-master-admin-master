4a6b557608117506962bad4679ea764136e6d6c8
AbdoNayer <abdelwahabnayer20@gmail.com>
2021-05-09 12:45:12 +0200

    change nav

---
+++ src/views/organizations/Organizations.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState, createRef } from 'react'
+
+
+const ThemeView = () => {
+  const [color, setColor] = useState('rgb(255, 255, 255)')
+  const ref = createRef()
+
+  
+}
+
+const Organizations = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Organizations
